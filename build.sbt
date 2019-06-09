5c0dfd2dec91c6f13f9faafc84c8033110186a99
D Cameron Mauch <cam@douglas-cameron.net>
2019-06-08 20:57:34 -0600

    adding initial http4s dependencies

--- build.sbt
+++ build.sbt
@@ -1,11 +1,14 @@
 import Dependencies.allDeps
 
-ThisBuild / scalaVersion := "2.13.0"
+ThisBuild / scalaVersion := "2.12.8"
 ThisBuild / organization := "com.github.dcameronmauch"
 ThisBuild / version      := "0.1.0"
 
 lazy val app = (project in file("."))
   .settings(
     name := "scala_http4s_zio",
-    libraryDependencies ++= allDeps
+    libraryDependencies ++= allDeps,
+    scalacOptions ++= Seq(
+      "-Ypartial-unification"
+    )
   )
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,9 +1,18 @@
 import sbt._
 
 object Dependencies {
-  private val mainDeps: Seq[ModuleID] = Seq()
+  private val http4s_version = "0.20.1"
+  private val http4s_blaze_server = "org.http4s" %% "http4s-blaze-server" % http4s_version
+  private val http4s_dsl = "org.http4s" %% "http4s-dsl" % http4s_version
 
-  private val testDeps: Seq[ModuleID] = Seq()
+  // private val specs2 = "org.specs2" %% "specs2-core" % "4.5.1"
+
+  private val mainDeps = Seq(
+    http4s_blaze_server,
+    http4s_dsl
+  )
+
+  private val testDeps = Seq.empty[ModuleID]
 
   val allDeps: Seq[ModuleID] = mainDeps ++ testDeps.map(_ % Test)
 }
