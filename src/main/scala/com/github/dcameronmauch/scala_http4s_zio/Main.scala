8ec29bfec01bf17f89e93e0f0da0a8b7c1f3c304
D Cameron Mauch <cam@douglas-cameron.net>
2019-07-20 16:19:09 -0600

    work in progress

--- src/main/scala/com/github/dcameronmauch/scala_http4s_zio/Main.scala
+++ src/main/scala/com/github/dcameronmauch/scala_http4s_zio/Main.scala
@@ -1,5 +1,44 @@
 package com.github.dcameronmauch.scala_http4s_zio
 
-object Main extends App {
-  println("hello world")
+import cats.effect._
+import cats.implicits._
+import org.http4s._
+import org.http4s.dsl.io._
+import org.http4s.implicits._
+import org.http4s.server.blaze._
+
+
+//import cats.effect._
+//
+//import io.circe._
+//import io.circe.generic.auto._
+//import io.circe.syntax._
+//
+//import org.http4s.circe._
+
+object Main extends IOApp {
+
+  case class Request(name: String)
+
+  object Response {
+    def apply(request: Request): Response =
+      Response(s"Hello ${request.name}!")
+  }
+
+  case class Response(greeting: String)
+
+  private val service = HttpRoutes.of[IO] {
+    case json @ POST -> Root / "hello" / name => for {
+      request <- json.as[Request]
+      response <- Ok(Response(request).asJson)
+    } yield response
+  }.orNotFound
+
+  def run(args: List[String]): IO[ExitCode] =
+    BlazeServerBuilder[IO]
+      .bindHttp(8080, "localhost")
+      .withHttpApp(service)
+      .resource
+      .use(_ => IO.never)
+      .as(ExitCode.Success)
 }
